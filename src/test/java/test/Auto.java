1f58ecda709ce92ebeb73589afc2b283b4163d8c
Juanpa0128j <juanpa0128j@gmail.com>
2023-02-17 10:03:07 -0500

    Update Auto.java

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -9,17 +9,12 @@ public class Auto {
 	int registro;
 	static int cantidadCreados;
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-	
-	public int cantidadAsientos() {
+	int cantidadAsientos() {
 		return (this.asientos.length);
 		
 	}
 	
-	public String verificarIntegridad() {	
+	String verificarIntegridad() {	
 		String PS = "Las piezas no son originales";
 		for (Asiento i : this.asientos) {
 			if (this.registro == i.registro && this.registro == this.motor.registro && this.motor.registro == i.registro)
