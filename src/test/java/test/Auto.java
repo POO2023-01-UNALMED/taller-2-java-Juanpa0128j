4faff18a4d1e41a323e5b46706e0007c9a642b32
Juanpa0128j <juanpa0128j@gmail.com>
2023-02-17 09:43:05 -0500

    1st commit

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,32 @@
+package test;
+public class Auto {
+	String modelo;
+	int precio;
+	Asiento asientos[] = new Asiento[4];
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public int cantidadAsientos() {
+		return (this.asientos.length);
+		
+	}
+	
+	public String verificarIntegridad() {	
+		String PS = null;
+		for (Asiento i : this.asientos) {
+			if (this.registro == i.registro && this.registro == this.motor.registro && this.motor.registro == i.registro)
+				PS = "Auto original";
+			else
+				PS = "Las piezas no son originales";
+			}
+		
+		return PS;
+	}
+}
